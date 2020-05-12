0b98475be653433c5898b348ed042f69ca1f2b45
julie-norris <julienorris0715@gmail.com>
2020-05-11 20:16:10 -0700

    finished model

--- StepUp_server.py
+++ StepUp_server.py
@@ -7,7 +7,7 @@ import json
 import pandas as pd
 from pandas import ExcelFile
 from pandas import ExcelWriter
-from flask_debugtoolbar import DebugToolbarExtension
+#from flask_debugtoolbar import DebugToolbarExtension
 #con_str = 'psnavigator/navigate@xxxxxxx/xxxx'
 #con = cx_Oracle.connect(con_str)
 #c = con.cursor()
@@ -25,64 +25,65 @@ def home():
 def register():
 	"""Creates a new user in the database."""
 
-    provider_organization = request.form["provider_organization"]
-    email = request.form["email"]
-    password = request.form["password"]
-    fname = request.form["fname"]
-    lname = request.form["lname"]
-    phone = request.form["phone_number"]
-    
+	provider_organization = request.form["provider_organization"]
+	email = request.form["email"]
+	password = request.form["password"]
+	fname = request.form["fname"]
+	lname = request.form["lname"]
+	phone = request.form["phone_number"]
+	
 
-    #match = re.match(r'^[a-zA-Z0-9 -]{5,9}$', license_number)
+	#match = re.match(r'^[a-zA-Z0-9 -]{5,9}$', license_number)
 """
-    #if not match:
-    #    flash("Invalid license number. Try again.")
-    #    return redirect("/register")
+	#if not match:
+	#    flash("Invalid license number. Try again.")
+	#    return redirect("/register")
 
-   """ user = Person.query.filter_by(email=email).first()
+   """ 
+	#user = Person.query.filter_by(email=email).first()
 
-    if user:
-        flash("You have already registered! Please log-in!")
-        return redirect("/login")
+"""	if user:
+		flash("You have already registered! Please log-in!")
+		return redirect("/login")
 
-    else:    
-        user = Person(
-            email=email,
-            password=password,
-            fname=fname,
-            lname=lname,
-            phone=phone, 
-            organization=provider_organization
-            )
+	else:    
+		user = Person(
+			email=email,
+			password=password,
+			fname=fname,
+			lname=lname,
+			phone=phone, 
+			organization=provider_organization
+			)
 
-    db.session.add(user)
-    db.session.commit()
+	db.session.add(user)
+	db.session.commit()
 
 #  NEED SOMETHING HERE TO MAKE SURE THEY ARE LOGGED IN -- IS THIS ENOUGH:
-    session["user_id"] = user.user_id 
-    
-    flash("Thank you for registering for CarPool! You have been logged in!")
-    
-    if driver_or_rider == 'driver':
-        return redirect("/driver")
-    else:
-        return redirect("/rider")
-"""
+	session["user_id"] = user.user_id 
+	
+	flash("Thank you for registering for CarPool! You have been logged in!")
+	
+	if driver_or_rider == 'driver':
+		return redirect("/driver")
+	else:
+		return redirect("/rider")
+
 @app.route("/add_student")
 def add_student(): 
-	"""user can add a student to an existing group or add a student to a new group"""
+	user can add a student to an existing group or add a student to a new group
 
 	return render_template("add_student.html")
 
 @app.route("/student_groups")
 def student_groups():
 	
-	"""When user logs in - the code will query student groups associated with
+	When user logs in - the code will query student groups associated with
 	the user displaying data available for each student in a chart ie. number of sessions attended
-	number of sessions missed, interventiont type, start date, end date."""
+	number of sessions missed, interventiont type, start date, end date.
 
-	"""when the student name is clicked the browser opens up a tab specific to the student
-	with graphics (?) and starting criteria, data collected durng intervention, end goal"""
+	when the student name is clicked the browser opens up a tab specific to the student
+	with graphics (?) and starting criteria, data collected durng intervention, end goal
 
 	return render_template("student_groups.html")
 
@@ -90,20 +91,20 @@ def student_groups():
 @app.route("/notifictions")
 def notifications():
 
-	""" boilerplate notifications that will be sent to relevant parties when
+	 boilerplate notifications that will be sent to relevant parties when
 	1. student attends a session
 	2. student misses a session
 	3. student achieves goal
 	4. student exits intervention without achieving goal
 	5. other(?)
-	"""
+	
 
-	return render_template("notifications.html")
+	return render_template("notifications.html")"""
 
 
 if __name__ == "__main__":
-    #connect_to_db(app)
-    app.run(host='127.0.0.1')
+	#connect_to_db(app)
+	app.run(host='127.0.0.1')
 
 
 
--- model.py
+++ model.py
@@ -3,122 +3,124 @@ from flask_sqlalchemy import flask_sqlalchemy
 db = SQLAlchemy()
 
 class Users(db.Model):
-  """stores information to identify user"""
-  __tablename__ = "users"
+	"""stores information to identify user"""
+	__tablename__ = "users"
 
-  user_id = db.Column(db.Integer,
-                      autoincrement = True
-                      primary_key = True) # auto-increment
-  
-  email = db.Column(db.string(64), nullable=False)
-  user_type = db.Column(db.string(20), nullable=False)
-  organization = db.Column(db.string(64), nullable=False)
-  provider_id= db.Column(db.integer, db.ForeignKey(providers.provider_id))
+	user_id = db.Column(db.Integer,
+											autoincrement = True
+											primary_key = True) # auto-increment
+	
+	email = db.Column(db.string(64), nullable=False)
+	user_type = db.Column(db.string(20), nullable=False)
+	organization = db.Column(db.string(64), nullable=False)
+	provider_id= db.Column(db.integer, db.ForeignKey(providers.provider_id))
 
 
 class InterventionCycles (db.Model):
-  """stores all cycles of intervention provided to students"""
-  __tablename__ = "cycles"
+	"""stores all cycles of intervention provided to students"""
+	__tablename__ = "cycles"
 
 
-  intervention_id = db.Column(db.Integer,
-                              autoincrement = True
-                              primary_key = True)
+	intervention_id = db.Column(db.Integer,
+															autoincrement = True
+															primary_key = True)
 
-  start_date = db.Column(db.DateTime, nullable=False)
+	start_date = db.Column(db.DateTime, nullable=False)
+	end_date = db.Column(db.DateTime, nullable=False)
+	goal = db.Column(db.string(120), nullable=False)
+	intervention_type = db.Column(db.string(20), nullable=False)
+	goal = db.Column(db.string(240), nullable=True)
+	summative_data = db.Column(db.numeric(6,2), nullable-False)
+	comment = db.Column(db.string(120), nullable=False)
+	grade_level = db.Column(db.Integer, nullable=False)
+	student_id = db.Column(db.integer,
+												 db.ForeignKey(students.student_number)) # inline relationship (many-to-one)
 
-  end_date = db.Column(db.DateTime, nullable=False)
-  goal = db.Column(db.string(64), nullable=False)
-  intervention_type = db.Column(db.string(20), nullable=False)
-  comment = db.Column(db.string(120), nullable=False)
-  grade = db.Column(db.Integer, nullable=False)
-  student_id = db.Column(db.integer,
-                         db.ForeignKey(students.student_number)) # inline relationship (many-to-one)
-  #provider = db.Column(db.string(100),
-                      #   db.ForeignKey(providers.organization)) # one to one (one incident has one provider)
 
 class Students(db.Model):
-  """stores information of students receiving interventions"""
-  __tablename__ = "students" 
+	"""stores information of students receiving interventions"""
+	__tablename__ = "students" 
 
-  student_number = db.Column(db.Integer,
-                            primary_key=True)
-  stu_fname = db.Column(db.string(20), nullable=False)
-  stu_lname = db.Column(db.string(20), nullable=False)
-  previous_intervention = db.Column(db.integer,
-                                    db.ForeignKey(cycles.intervention_id)) # one-to-many
-  teacher = db.Colum(db.string(20), nullable=True)
+	student_number = db.Column(db.Integer,
+														primary_key=True)
+	stu_fname = db.Column(db.string(20), nullable=False)
+	stu_lname = db.Column(db.string(20), nullable=False)
+	previous_intervention = db.Column(db.integer,
+																		db.ForeignKey(cycles.intervention_id)) # one-to-many
+	gpa = db.column(db.float, nullable=True)
+	schoolid = db.column(db.integer, nullable=False)
+	teacher = db.Colum(db.string(20), nullable=True)
+	guardian = db.Column(db.string(64), nullable=True)
+	SchoolAdmin = db.Column(db.string(64), nullable=True)
+	Student_email = db.Column(db.string(64), nullable=True)
+	isSpEd = db.Column(db.string(5), nullable=True)
+	isEL = db.Column(db.string(5), nullable=True)
+	isHomeless = db.Column(db.string(5), nullable=True)
+	isFosterYouth = db.Column(db.string(5), nullable=True)
+
+
 
 class ProviderOrgs(db.Model):
-  """stores information about intervention providers"""
-  __tablename__ = "providers" 
+	"""stores information about intervention providers"""
+	__tablename__ = "providers" 
 
-  provider_id = db.Column(db.Integer,
-                            autoincrement = True
-                            primary_key=True)
-  organization_name = db.Column(db.string(64), nullable=False)
-  provider_email = db.Column(db.string(64), nullable=True)
-  provider_phone = db.Column(db.integer, nullable=True)
-  organization_phone = db.Column(db.string(20), nullable=False)
+	provider_id = db.Column(db.Integer,
+														autoincrement = True
+														primary_key=True)
+	organization_name = db.Column(db.string(64), nullable=False)
+	provider_name  = db.Column(db.string(64), nullable=True)
+	provider_email = db.Column(db.string(64), nullable=True)
+	organization_phone = db.Column(db.string(20), nullable=False)
 
 
 
 class StudentGroups(db.Model):
-    """stores information about students in a group receiving the same intervnetion"""
-  __tablename__ = "student_group"  
-  group_id = db.Column(db.Integer,
-                        autoincrement = True,
-                        primary_key=True)
-  student_number = db.Column(db.integer,
-                      db.ForeignKey(students.student_number))
-  provider_id = db.Column(db.integer,
-                      db.ForeignKey(providers.provider_id))
-  group_name = db.Column(db.string(64), nullable = True)
-
-class InterventionTypes(db.Model):
-    __tablename__ = "intervention_type"
-
-    type_id = db.Column(db.Integer,
-                        autoincrement = True,
-                        primary_key=True)
-    classification = db.Column(db.string(20), nullable=False) # pull down menu with predetermined labels and option for 'other'
+		"""stores information about students in a group receiving the same intervnetion"""
+	__tablename__ = "student_group"  
+	
+	group_id = db.Column(db.Integer,
+												autoincrement = True,
+												primary_key=True)
+	student_number = db.Column(db.integer,
+											db.ForeignKey(students.student_number))
+	group_current_intervention_id = db.Column(db.integer,
+											db.ForeignKey(current_intervention.current_id))
+	group_name = db.Column(db.string(64), nullable = True)
 
 
 class CurrentIntervention (db.Model):
-    __tablename__ = "current_group"
+		__tablename__ = "current_intervention"
 
-    current_id = db.Column(db.Integer,
-                        autoincrement = True,
-                        primary_key=True)
-    intervention_id = db.Column(db.integer,
-                      db.ForeignKey(cycles.intervention_id))
-    formative_data = db.Column(db.float,
-                                nullable=False)
-    type_id = db.Column(db.integer,
-                  db.ForeignKey(intervention_type.type_id))
-    provider_id =db.Column(db.integer,
-                          db.ForeignKey(providers.provider_id))
-    group_id =db.Column(db.integer,
-                                    db.ForeignKey(student_groups.group_id))
+		current_id = db.Column(db.Integer,
+												autoincrement = True,
+												primary_key=True)
+		current_intervention_id = db.Column(db.integer,
+											db.ForeignKey(cycles.intervention_id))
+		formative_data = db.Column(db.numeric(6,2),
+																nullable=False)
+		current_provider_id =db.Column(db.integer,
+													db.ForeignKey(providers.provider_id))
+		current_group_id =db.Column(db.integer,
+													db.ForeignKey(student_groups.group_id))
 
 
 
 #########################################################################
 #Helper Functions
 
-def connect_to_db(app, database_uri='postgresql:///stepup'):
+def connect_to_db(app, database_uri='postgresql:///mtss'):
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.app = app
-    db.init_app(app)
+		app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+		# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+		db.app = app
+		db.init_app(app)
 
 if __name__ == "__main__":
 
-    from server import app
-    connect_to_db(app)
+		from server import app
+		connect_to_db(app)
 
-    db.create_all()
+		db.create_all()
 
 
 
